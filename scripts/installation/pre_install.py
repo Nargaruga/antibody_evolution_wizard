753c3923631671e274c4210fb5fa1a7e1d49e063
Leonardo Da Pozzo <l.dapozzo398@gmail.com>
2025-04-03 22:18:29 +0200

    only create efficient-evolution env on linux
    On Windows we use a Docker container due to Linux-only dependencies

--- scripts/installation/pre_install.py
+++ scripts/installation/pre_install.py
@@ -11,44 +11,45 @@ def main():
     else:
         prefix = []
 
-    try:
-        subprocess.run(
-            "conda list --name efficient-evolution",
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.STDOUT,
-            shell=True,
-        ).check_returncode()
-    except subprocess.CalledProcessError:
+    if os.name == "posix":
+        try:
+            subprocess.run(
+                "conda list --name efficient-evolution",
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT,
+                shell=True,
+            ).check_returncode()
+        except subprocess.CalledProcessError:
+            subprocess.run(
+                prefix
+                + [
+                    "conda",
+                    "env",
+                    "create",
+                    "--name",
+                    "efficient-evolution",
+                    "-f",
+                    "environment.yml",
+                ],
+                cwd=os.path.join(wizard_root, "ext", "efficient-evolution"),
+                check=True,
+            )
+
         subprocess.run(
             prefix
             + [
                 "conda",
-                "env",
-                "create",
-                "--name",
+                "run",
+                "-n",
                 "efficient-evolution",
-                "-f",
-                "environment.yml",
+                "pip",
+                "install",
+                "-e",
+                ".",
             ],
             cwd=os.path.join(wizard_root, "ext", "efficient-evolution"),
             check=True,
         )
-
-    subprocess.run(
-        prefix
-        + [
-            "conda",
-            "run",
-            "-n",
-            "efficient-evolution",
-            "pip",
-            "install",
-            "-e",
-            ".",
-        ],
-        cwd=os.path.join(wizard_root, "ext", "efficient-evolution"),
-        check=True,
-    )
 
 
 if __name__ == "__main__":
