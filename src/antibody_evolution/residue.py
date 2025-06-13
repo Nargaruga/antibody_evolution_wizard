567bc0298690363beb0646b4c776c8ce045bec10
Leonardo Da Pozzo <l.dapozzo398@gmail.com>
2025-06-13 13:24:20 +0200

    allow mapping from aminoacid threeletter to onelettere

--- src/antibody_evolution/residue.py
+++ src/antibody_evolution/residue.py
@@ -27,6 +27,29 @@ RESIDUE_NAME_MAPPING = {
     "M": "MET",
 }
 
+RESIDUE_NAME_REVERSE_MAPPING = {
+    "CYS": "C",
+    "ASP": "D",
+    "SER": "S",
+    "GLN": "Q",
+    "LYS": "K",
+    "ILE": "I",
+    "PRO": "P",
+    "THR": "T",
+    "PHE": "F",
+    "ASN": "N",
+    "GLY": "G",
+    "HIS": "H",
+    "LEU": "L",
+    "ARG": "R",
+    "TRP": "W",
+    "ALA": "A",
+    "VAL": "V",
+    "GLU": "E",
+    "TYR": "Y",
+    "MET": "M",
+}
+
 
 def one_to_three(oneletter):
     """Convert a single letter amino acid code to a three letter code."""
@@ -36,13 +59,25 @@ def one_to_three(oneletter):
 
     return RESIDUE_NAME_MAPPING[oneletter]
 
+
+def three_to_one(threeletter):
+    """Convert a three letter amino acid code to a single letter code."""
+
+    if threeletter not in RESIDUE_NAME_REVERSE_MAPPING:
+        raise ValueError(f"Invalid three letter code: {threeletter}")
+
+    return RESIDUE_NAME_REVERSE_MAPPING[threeletter]
+
+
 def resn_exists(resn):
     """Check if a residue name exists in the mapping."""
     return resn in RESIDUE_NAME_MAPPING.values()
 
+
 def oneletter_exists(oneletter):
     """Check if a one-letter code exists in the mapping."""
     return oneletter in RESIDUE_NAME_MAPPING.keys()
+
 
 @dataclass
 class Residue:
